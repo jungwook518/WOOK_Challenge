e42d72467804003987cc0fb054b3e38e30bd694a
HJW <hju0518@sogang.ac.kr>
2021-12-29 17:42:58 +0900

    dd

--- base_model/base_model.py
+++ base_model/base_model.py
@@ -93,42 +93,110 @@ class Base_model(nn.Module):
 ###############################################
 #####          Transformer         ############
 ###############################################
+class Swish(nn.Module):
+    """
+    Swish is a smooth, non-monotonic function that consistently matches or outperforms ReLU on deep networks applied
+    to a variety of challenging domains such as Image classification and Machine translation.
+    """
 
-class VGGExtractor_fairseq(nn.Module):
+    def __init__(self):
+        super(Swish, self).__init__()
 
-    def __init__(
-            self,
-            input_dim: int,
-            in_channels: int = 1,
-            out_channels: int or tuple = (64, 128),
-            activation: str = 'relu',
-    ):
-        super(VGGExtractor_fairseq, self).__init__()
-        self.in_channels = in_channels
+    def forward(self, inputs):
+        return inputs * inputs.sigmoid()
+        
+class MaskCNN(nn.Module):
+    """
+    Masking Convolutional Neural Network
+    Adds padding to the output of the module based on the given lengths.
+    This is to ensure that the results of the model do not change when batch sizes change during inference.
+    Input needs to be in the shape of (batch_size, channel, hidden_dim, seq_len)
+    Refer to https://github.com/SeanNaren/deepspeech.pytorch/blob/master/model.py
+    Copyright (c) 2017 Sean Naren
+    MIT License
+    Args:
+        sequential (torch.nn): sequential list of convolution layer
+    Inputs: inputs, seq_lengths
+        - **inputs** (torch.FloatTensor): The input of size BxCxHxT
+        - **seq_lengths** (torch.IntTensor): The actual length of each sequence in the batch
+    Returns: output, seq_lengths
+        - **output**: Masked output from the sequential
+        - **seq_lengths**: Sequence length of output from the sequential
+    """
+    def __init__(self, sequential: nn.Sequential):
+        super(MaskCNN, self).__init__()
+        self.sequential = sequential
+
+    def forward(self, inputs: Tensor, seq_lengths: Tensor):
+        output = None
+
+        for module in self.sequential:
+            output = module(inputs)
+            mask = torch.BoolTensor(output.size()).fill_(0)
+
+            if output.is_cuda:
+                mask = mask.cuda()
+
+            seq_lengths = self._get_sequence_lengths(module, seq_lengths)
+
+            for idx, length in enumerate(seq_lengths):
+                length = length.item()
+
+                if (mask[idx].size(2) - length) > 0:
+                    mask[idx].narrow(dim=2, start=length, length=mask[idx].size(2) - length).fill_(1)
+
+            output = output.masked_fill(mask, 0)
+            inputs = output
+
+        return output, seq_lengths
+
+    def _get_sequence_lengths(self, module: nn.Module, seq_lengths: Tensor):
+        """
+        Calculate convolutional neural network receptive formula
+        Args:
+            module (torch.nn.Module): module of CNN
+            seq_lengths (torch.IntTensor): The actual length of each sequence in the batch
+        Returns: seq_lengths
+            - **seq_lengths**: Sequence length of output from the module
+        """
+        if isinstance(module, nn.Conv2d):
+            numerator = seq_lengths + 2 * module.padding[1] - module.dilation[1] * (module.kernel_size[1] - 1) - 1
+            seq_lengths = numerator.float() / float(module.stride[1])
+            seq_lengths = seq_lengths.int() + 1
+
+        elif isinstance(module, nn.MaxPool2d):
+            seq_lengths >>= 1
+
+        return seq_lengths.int()
+
+class Conv2dExtractor(nn.Module):
+    """
+    Provides inteface of convolutional extractor.
+    Note:
+        Do not use this class directly, use one of the sub classes.
+        Define the 'self.conv' class variable.
+    Inputs: inputs, input_lengths
+        - **inputs** (batch, time, dim): Tensor containing input vectors
+        - **input_lengths**: Tensor containing containing sequence lengths
+    Returns: outputs, output_lengths
+        - **outputs**: Tensor produced by the convolution
+        - **output_lengths**: Tensor containing sequence lengths produced by the convolution
+    """
+    supported_activations = {
+        'hardtanh': nn.Hardtanh(0, 20, inplace=True),
+        'relu': nn.ReLU(inplace=True),
+        'elu': nn.ELU(inplace=True),
+        'leaky_relu': nn.LeakyReLU(inplace=True),
+        'gelu': nn.GELU(),
+        'swish': Swish(),
+    }
+
+    def __init__(self, input_dim: int, activation: str = 'hardtanh'):
+        super(Conv2dExtractor, self).__init__()
         self.input_dim = input_dim
-        self.out_channels = out_channels
-        self.conv = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels[0], kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(num_features=out_channels[0]),
-                nn.ReLU(),
-                nn.Conv2d(out_channels[0], out_channels[0], kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(num_features=out_channels[0]),
-                nn.ReLU(),
-                nn.MaxPool2d(2, stride=2, ceil_mode=True),
-                nn.Conv2d(out_channels[0], out_channels[1], kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(num_features=out_channels[1]),
-                nn.ReLU(),
-                nn.Conv2d(out_channels[1], out_channels[1], kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(num_features=out_channels[1]),
-                nn.ReLU(),
-                nn.MaxPool2d(2, stride=2, ceil_mode=True),
-            )
-    def get_output_dim(self):
-        if isinstance(self, VGGExtractor_fairseq):
-            output_dim = (self.input_dim - 1) << 5 if self.input_dim % 2 else self.input_dim << 5
+        self.activation = Conv2dExtractor.supported_activations[activation]
+        self.conv = None
 
-        return output_dim
-    
     def get_output_lengths(self, seq_lengths: Tensor):
         assert self.conv is not None, "self.conv should be defined"
 
@@ -143,16 +211,167 @@ class VGGExtractor_fairseq(nn.Module):
 
         return seq_lengths.int()
 
+    def get_output_dim(self):
+        if isinstance(self, VGGExtractor):
+            output_dim = (self.input_dim - 1) << 5 if self.input_dim % 2 else self.input_dim << 5
+
+        elif isinstance(self, DeepSpeech2Extractor):
+            output_dim = int(math.floor(self.input_dim + 2 * 20 - 41) / 2 + 1)
+            output_dim = int(math.floor(output_dim + 2 * 10 - 21) / 2 + 1)
+            output_dim <<= 5
+
+        elif isinstance(self, Conv2dSubsampling):
+            factor = ((self.input_dim - 1) // 2 - 1) // 2
+            output_dim = self.out_channels * factor
+
+        else:
+            raise ValueError(f"Unsupported Extractor : {self.extractor}")
+
+        return output_dim
+
     def forward(self, inputs, input_lengths):
-        # pdb.set_trace()
-        outputs = self.conv(inputs.unsqueeze(1))
-        
-        output_lengths = self.get_output_lengths(input_lengths)
+        """
+        inputs: torch.FloatTensor (batch, time, dimension)
+        input_lengths: torch.IntTensor (batch)
+        """
+        outputs, output_lengths = self.conv(inputs.unsqueeze(1).transpose(2, 3), input_lengths)
 
         batch_size, channels, dimension, seq_lengths = outputs.size()
         outputs = outputs.permute(0, 3, 1, 2)
         outputs = outputs.view(batch_size, seq_lengths, channels * dimension)
+
         return outputs, output_lengths
+
+class Conv2dSubsampling(Conv2dExtractor):
+    """
+    Convolutional 2D subsampling (to 1/4 length)
+    Args:
+        input_dim (int): Dimension of input vector
+        in_channels (int): Number of channels in the input vector
+        out_channels (int): Number of channels produced by the convolution
+        activation (str): Activation function
+    Inputs: inputs
+        - **inputs** (batch, time, dim): Tensor containing sequence of inputs
+        - **input_lengths** (batch): list of sequence input lengths
+    Returns: outputs, output_lengths
+        - **outputs** (batch, time, dim): Tensor produced by the convolution
+        - **output_lengths** (batch): list of sequence output lengths
+    """
+    def __init__(
+            self,
+            input_dim: int,
+            in_channels: int,
+            out_channels: int,
+            activation: str = 'relu',
+    ):
+        super(Conv2dSubsampling, self).__init__(input_dim, activation)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.conv = MaskCNN(
+            nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2),
+                self.activation,
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2),
+                self.activation,
+            )
+        )
+
+    def forward(self, inputs: Tensor, input_lengths: Tensor):
+        outputs, input_lengths = super().forward(inputs, input_lengths)
+        output_lengths = input_lengths >> 2
+        output_lengths -= 1
+        return outputs, output_lengths
+
+
+class DeepSpeech2Extractor(Conv2dExtractor):
+    """
+    DeepSpeech2 extractor for automatic speech recognition described in
+    "Deep Speech 2: End-to-End Speech Recognition in English and Mandarin" paper
+    - https://arxiv.org/abs/1512.02595
+    Args:
+        input_dim (int): Dimension of input vector
+        in_channels (int): Number of channels in the input vector
+        out_channels (int): Number of channels produced by the convolution
+        activation (str): Activation function
+    Inputs: inputs, input_lengths
+        - **inputs** (batch, time, dim): Tensor containing input vectors
+        - **input_lengths**: Tensor containing containing sequence lengths
+    Returns: outputs, output_lengths
+        - **outputs**: Tensor produced by the convolution
+        - **output_lengths**: Tensor containing sequence lengths produced by the convolution
+    """
+    def __init__(
+            self,
+            input_dim: int,
+            in_channels: int = 1,
+            out_channels: int = 32,
+            activation: str = 'hardtanh',
+    ) :
+        super(DeepSpeech2Extractor, self).__init__(input_dim=input_dim, activation=activation)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.conv = MaskCNN(
+            nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=(41, 11), stride=(2, 2), padding=(20, 5), bias=False),
+                nn.BatchNorm2d(out_channels),
+                self.activation,
+                nn.Conv2d(out_channels, out_channels, kernel_size=(21, 11), stride=(2, 1), padding=(10, 5), bias=False),
+                nn.BatchNorm2d(out_channels),
+                self.activation,
+            )
+        )
+
+    def forward(self, inputs: Tensor, input_lengths: Tensor):
+        return super().forward(inputs, input_lengths)
+
+class VGGExtractor(Conv2dExtractor):
+    """
+    VGG extractor for automatic speech recognition described in
+    "Advances in Joint CTC-Attention based End-to-End Speech Recognition with a Deep CNN Encoder and RNN-LM" paper
+    - https://arxiv.org/pdf/1706.02737.pdf
+    Args:
+        input_dim (int): Dimension of input vector
+        in_channels (int): Number of channels in the input image
+        out_channels (int or tuple): Number of channels produced by the convolution
+        activation (str): Activation function
+    Inputs: inputs, input_lengths
+        - **inputs** (batch, time, dim): Tensor containing input vectors
+        - **input_lengths**: Tensor containing containing sequence lengths
+    Returns: outputs, output_lengths
+        - **outputs**: Tensor produced by the convolution
+        - **output_lengths**: Tensor containing sequence lengths produced by the convolution
+    """
+    def __init__(
+            self,
+            input_dim: int,
+            in_channels: int = 1,
+            out_channels: int or tuple = (64, 128),
+            activation: str = 'relu',
+    ):
+        super(VGGExtractor, self).__init__(input_dim=input_dim, activation=activation)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.conv = MaskCNN(
+            nn.Sequential(
+                nn.Conv2d(in_channels, out_channels[0], kernel_size=3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(num_features=out_channels[0]),
+                self.activation,
+                nn.Conv2d(out_channels[0], out_channels[0], kernel_size=3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(num_features=out_channels[0]),
+                self.activation,
+                nn.MaxPool2d(2, stride=2),
+                nn.Conv2d(out_channels[0], out_channels[1], kernel_size=3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(num_features=out_channels[1]),
+                self.activation,
+                nn.Conv2d(out_channels[1], out_channels[1], kernel_size=3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(num_features=out_channels[1]),
+                self.activation,
+                nn.MaxPool2d(2, stride=2),
+            )
+        )
+
+    def forward(self, inputs: Tensor, input_lengths: Tensor):
+        return super().forward(inputs, input_lengths)
 
 class ResidualConnectionModule(nn.Module):
     """
@@ -362,7 +581,7 @@ class AVTransformerEncoder(nn.Module):
         self.input_dim = input_dim
         self.d_ff = d_ff
         self.dropout_p = dropout_p
-        self.extractor = VGGExtractor_fairseq(input_dim)
+        self.extractor = VGGExtractor(input_dim)
         self.conv_output_dim = self.extractor.get_output_dim()
 
         self.input_proj = Linear(self.conv_output_dim, d_model)
